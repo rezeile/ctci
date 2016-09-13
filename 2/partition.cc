c6a41e64ff0704241446e1505579f5b0efb91b2a
Eliezer Abate <code.eliezer@gmail.com>
2016-09-13 10:59:25 -0700

    completed partition problem

--- 2/partition.cc
+++ 2/partition.cc
@@ -1,15 +1,47 @@
 #include "node.h"
+#include "list.h"
+#include <string>
+#include <sstream>
+#include <iostream>
+using namespace std;
 
-
-void partition(int x, node *head) {
-	node* bigger, bigger_tail;
-	node* smaller, smaller_tail;
+void partition(int x, node* &head) {
+	node* bigger = nullptr, *bigger_tail = nullptr;
+	node* smaller = nullptr, *smaller_tail = nullptr;
 	node *cur = head;
 
 	while(cur != nullptr) {
 		if(cur->data < x) {
-			if(smaller) {
-			cl	
+			if(!smaller) {
+		 		smaller = smaller_tail = cur;
+			} else {
+				smaller_tail = smaller_tail->next = cur;
+			}
+			cur = cur->next;
+			smaller_tail->next = nullptr;
+		} else {
+			if(!bigger) {
+		 		bigger = bigger_tail = cur;
+			} else {
+				bigger_tail = bigger_tail->next = cur;
+			}
+			cur = cur->next;
+			bigger_tail->next = nullptr;
 		}
 	}
+	
+	smaller_tail->next = bigger;
+	head = smaller;
+}
+
+int main() {
+	string s = "8 6 4 2";
+	stringstream ss(s);
+	node *list = construct_list(ss);
+	cout << "Before partition" << endl;	
+	print_node(list);
+	cout << "After partition" << endl;
+	partition(5,list);
+	print_node(list);
+	return 0;
 }
