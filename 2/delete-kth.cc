d9435f5ec846bacfbc0f90eac81dd5eca1b2ee30
Eliezer Abate <code.eliezer@gmail.com>
2016-09-15 17:34:37 -0700

    implemented kth to last node iteratively using the clever two pointer mechanism that is similar to Floyd's cycle detection algorithm

--- 2/delete-kth.cc
+++ 2/delete-kth.cc
@@ -1,17 +1,21 @@
+#include <string>
+#include <sstream>
+#include <iostream>
 #include "node.h"
 #include "list.h"
+using namespace std;
 
 typedef unsigned int uint;
 // function prototype
-void helper(node *list,uint &p,uint k,node *first);
+void helper(node*& list,uint &p,uint k,node *first);
 
-void delete_kth(node *list,uint k) {
+void delete_kth(node*& list,uint k) {
 	uint pos = 0;
 	node *first = list;
 	helper(list,pos,k,first);
 }
 
-void helper(node *list,uint &p,uint k,node *first) {
+void helper(node*& list,uint &p,uint k,node *first) {
 	if(list == nullptr) return;
 
 	helper(list->next,p,k,first);
@@ -26,7 +30,33 @@ void helper(node *list,uint &p,uint k,node *first) {
 	}		
 }
 
+void simpleTest() {
+	string s1 = "3 2 1";
+	stringstream ss1(s1);
+	node *list = construct_list(ss1);
+	cout << "Before deletion: " << endl;
+	print_node(list);
+	cout << "After deletion: " << endl;
+    delete_kth(list,3);
+	print_node(list);
+}
+
+/* delete a single element */
+void mediumTest() {
+	string s1 = "1";
+	stringstream ss1(s1);
+	node *list = construct_list(ss1);
+	cout << "Before deletion: " << endl;
+	print_node(list);
+	cout << "After deletion: " << endl;
+    delete_kth(list,1);
+	print_node(list);
+
+}
+
 int main() {
+	simpleTest();
+	mediumTest();
 	return 0;
 }
 
--- 2/kth_to_last.cc
+++ 2/kth_to_last.cc
@@ -3,7 +3,7 @@
 #include <cassert>
 int get_count(node *head);
 node *get_kth_to_last(node *head, int k);
-
+node *get_kth_to_last2(node *head, int k);
 int main() {
 	std::cout << "Enter a list: ";
 	string str;
@@ -15,7 +15,7 @@ int main() {
 		/* print output */
 		cout << "2nd to last node: " << endl;
 		cout << "expected: 8" << endl;
-		node *k = get_kth_to_last(head,2);
+		node *k = get_kth_to_last2(head,2);
 		cout << "received: " << k->data << endl;
 		
 		/* reprompt */
@@ -50,4 +50,17 @@ node *get_kth_to_last(node *head,int k) {
 	return nullptr;
 }
 
+node *get_kth_to_last2(node *head, int k) {
+	node *p1 = head;
+	node *temp = head;
+	for(int i = 0; i < k; i++) 
+		temp = temp->next;
+	node *p2 = temp;
+
+	while( p2 != nullptr) {
+		p2 = p2->next;
+		p1 = p1->next;
+	}
+	return p1;
+}
 
