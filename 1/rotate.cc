bbed787e698692a5cf95e048bf1e797af6e569ad
Eliezer Abate <code.eliezer@gmail.com>
2016-09-17 16:44:00 -0700

    completed rotation in matrix rotation in place using O(1) space and with a run time of O(n^2)

--- 1/rotate.cc
+++ 1/rotate.cc
@@ -8,19 +8,66 @@ typedef unsigned int uint;
 // function prototypes 
 void rotate_outer(mtx &m,uint n);
 void rotate(mtx &m);
+void rotate2(mtx &m);
 
-int main() {
+void simplestTest() {
+	cout << "simplest test: " << endl;
+	mtx M(4,vector<int>(4));
+	populateMatrix(M);
+	printMatrix(M);
+	rotate2(M);
+	cout << "After Rotation: " << endl;
+	printMatrix(M);
+
+}
+
+void simpleTest() {
+	cout << "simple test: " << endl;
 	mtx M(6,vector<int>(6));
+	populateMatrix(M);
+	printMatrix(M);
+	rotate2(M);
+	cout << "After Rotation: " << endl;
+	printMatrix(M);
+}
+
+void mediumTest() {
+	cout << "medium test: " << endl;
+	mtx M(5,vector<int>(5));
 	populateMatrix(M);
 	printMatrix(M);
 	rotate(M);
 	cout << "After Rotation: " << endl;
 	printMatrix(M);
+}
+
+void rotate2(mtx &m) {
+	for(int i = m.size(); i >= 1; i -= 2) {
+		uint offset = (m.size() - i) / 2;
+		uint start = 0 + offset;
+		uint end = (m.size() - 1) - offset;
+		for(uint n = start; n < end; n++) {
+			int temp = m[start][n];
+			// ovewrite top by right 
+			m[start][n] = m[n][end];
+			// overwrite right by bottom
+			m[n][end] = m[end][end - n];
+			// overwrite bottom by left
+			m[end][end - n] = m[end - n][start];
+			// overwrite left by temp
+			m[end - n][start] = temp;
+		}
+	}
+}
+
+int main() {
+	simplestTest();
+	mediumTest();
 	return 0;
 }
 
 void rotate(mtx &m) {
-	for(uint i = m.size(); i >= 1; i -= 2) {
+	for(int i = m.size(); i >= 1; i -= 2) {
 		rotate_outer(m,i);
 	}
 }
@@ -31,25 +78,28 @@ void rotate_outer(mtx &m, uint n) {
 	uint end = (m.size() - 1) - offset;
 	
 	vector<int> temp(end - start);
-	vector<int> writer(end - 1);
-
+	vector<int> writer(end - start);
+	uint sub = start + 1;	
 	// 1st stage
+	for(uint i = start; i < end; i++)
+		writer[i - start] = m[start][i];
+	uint wend = writer.size() - 1;
 	for(uint i = start + 1; i <= end; i++) {
-		uint sub = start + 1;
-		writer[i - sub] = m[i][start];
-		m[i][start] = m[start][i - 1];
+		temp[i - sub] = m[i][start];
+		m[i][start] = writer[wend--];
 	}
+	writer = temp;
 	// 2nd stage
 	for(uint i = start + 1; i <= end; i++) {
-		uint sub = start + 1;
 		temp[i - sub] = m[end][i];
-		m[end][i] = writer[i - start];
+		m[end][i] = writer[i - sub];
 	}
 	writer = temp;
 	// 3rd stage
+	wend = writer.size() - 1;
 	for(uint i = start; i <= end - 1; i++) {
 		temp[i - start] = m[i][end];
-		m[i][end] = writer[i - start];
+		m[i][end] = writer[wend--];
 	}
 	writer = temp;
 	// 4th  stage
--- 11/TODO.md
+++
@@ -1,4 +0,0 @@
-NOTE  . IF ANY problem takes me more than 30 minutes I need to prompty 
- move on or elelst i will wast etoo much time
-
-I need to complete 11.1
