7e25f07f94b4a9b8803c01d1b6ff1f7bb0b22d00
Eliezer Abate <code.eliezer@gmail.com>
2016-09-25 14:27:51 -0700

    fixed up problem 4-2 solution and added comments

--- 4/balanced-bin-tree.cc
+++ 4/balanced-bin-tree.cc
@@ -3,10 +3,13 @@
  * is balanced and false otherwise. A balanced
  * tree is one where for every node, the left
  * and right sub nodes differ by at most one. 
+ * This file has two solutions for this problem:
+ * isBalanced and isBalanced2. 
+ * 
+ * Author: Eliezer Abate
+ * Last Updated: September 25, 2016
  */
  
-#include "node.h"
-#include "tests.h"
 #include <algorithm>
 #include <iostream>
 #include <cstdlib>
@@ -14,19 +17,20 @@
 
 using namespace std;
 
-/* 
- * print out the list of values in the tree
- * via an in order traversal.
- */
 
- void in_order_print(node *root) {
- 	if(root != nullptr) {
-		cout << root->val << " ";
-		in_order_print(root->left);
-		in_order_print(root->right);
-	}
- }
+/*===============================================================
+  					      SOLUTION 1
+ ===============================================================*/
+
+struct node {
+	unsigned int val;
+	node *left;
+	node *right;
+};
+
 /* 
+ * function: height
+ * ----------------------------------------
  * helper function that gets the height 
  * of a node. Note that the height of a node
  * is the number of edges between itself 
@@ -38,20 +42,28 @@ unsigned int height(node *root) {
 		return 1 + max(height(root->left),height(root->right));
 }
 
-bool is_balanced(node *root) {
+/* 
+ * function: isBalanced
+ * -----------------------------------------------------------
+ * Given the root to a binary tree this function returns true
+ * if the tree is balanced and false otherwise.
+ */
+bool isBalanced(node *root) {
 	if(root == nullptr) return true;
     int diff = height(root->left) - height(root->right);
 	if(abs(diff)> 1) return false;
-	return is_balanced(root->left) && is_balanced(root->right);
+	return isBalanced(root->left) && isBalanced(root->right);
 }
 
 /* 
+ * function: addChildren 
+ * ------------------------------------------------------------ 
  * creates a new node to the left and right subchild of a
  * a node. These values must be greater than or equal 
  * to zero. A negative value indicates that we are not 
  * adding a value to the node.
  */
-void add_children(node *root,int left_val, int right_val) {
+void addChildren(node *root,int left_val, int right_val) {
 	assert(root != nullptr);
 	if(left_val >= 0) {
 		root->left = new node;
@@ -67,7 +79,32 @@ void add_children(node *root,int left_val, int right_val) {
 	}
 }
 
-void add_children(TreeNode *root,int left_val, int right_val) {
+/*
+ * function: cleanUp
+ * ------------------------------------------------ 
+ * cleans up dynamically allocated memory.
+ */
+ void cleanUp(node *root) {
+ 	if(root == nullptr) return;
+	node *left_child = root->left;
+	node *right_child = root->right;
+	delete root;
+	cleanUp(left_child);
+	cleanUp(right_child);
+ }
+
+/*===============================================================
+ * 					      SOLUTION 1
+ *===============================================================*/
+
+struct TreeNode {
+	int val;
+	int height;
+	TreeNode *left;
+	TreeNode *right;
+};
+
+void addChildren(TreeNode *root,int left_val, int right_val) {
 	assert(root != nullptr);
 	if(left_val >= 0) {
 		root->left = new TreeNode;
@@ -96,23 +133,8 @@ bool isBalanced2(TreeNode *root) {
 		if(root->height > 1) return false;
 	}
 	return isBalanced2(root->left) && isBalanced2(root->right);
-	
-
 }
 
-/* 
- * clean up dynamically allocated memory 
- */
- void clean_up(node *root) {
- 	if(root == nullptr) return;
-	node *left_child = root->left;
-	node *right_child = root->right;
-	delete root;
-	clean_up(left_child);
-	clean_up(right_child);
- }
-
 int main() {
-	simple_tests2();
-	medium_tests2();
+	return 0;
 }
--- 4/path-exists.cc
+++ 4/path-exists.cc
@@ -1,36 +1,115 @@
-#include <vector>
-#include <cstdio>
+/* 
+ * Solution for problem 4.2 of Cracking the Coding Interview. The 
+ * reader is encouraged to purchase the 5th edition of the textbook
+ * to view the problem statement. 
+ *
+ * Author: Eliezer Abate
+ * Last Updated: September 26, 2016
+ */
+
+#include <vector>  // std::vector
+#include <utility> // std::pair
+#include <cstdlib> // std::exit
+#include <cstdio>  // std::scanf, std::printf, std::freopen
 using namespace std;
 
-/*struct GraphNode {
-	int value;
-	bool visited;
-	int weight;
-	vector<GraphNode> adjacent;
-};
+/*===============================================================
+  					      SOLUTION 
+ ===============================================================*/
 
-bool findViaDFS(GraphNode source,GraphNode target) {
-	source.visited = true;
+/* 
+ * We will choose to represent the input graph using an adjacency 
+ * list. For the following graph: 
+ *
+ *                    [0]
+ *                   /   \
+ * 				    /     \
+ *				   v       v
+ *				  [1] <----[2]
+ *				           /
+ *						  /
+ *					   	 v
+ *					    [3]
+ *	
+ *
+ * We would have the follwing input information:
+ * 
+ *   4
+ *   2 1 2
+ *   0
+ *   2 1 3
+ *   0
+ *
+ *
+ * The first line will contain a single number V (in this case 4) representing the 
+ * number of vertices in the graph. It will be followed by V lines containing the
+ * one or more numbers. The first number indicated how many neighbors the ith vertex 
+ * has. For example the 2nd vertex has two neighbors (1 and 3, respectively). Thus
+ * the entry on the 2nd line (using 0-indexed numbering) is 2 1 3.
+ */
+
+typedef vector<vector<pair<int,int>>> AdjList;
+
+/* function: findViaDFS(int s,int t, AdjList &graph, vector<bool> &visitedNodes);
+ * --------------------------------------------------------------------------------
+ * Given the index of a source vertex and a target
+ * vertex this function returns true if a path 
+ * exists between the source and the target. Otherwise
+ * it returns false.
+ */
+bool findViaDFS(int source,int target,AdjList &graph, vector<bool> &visitedNodes) {
+	visitedNodes[source] = true;
 	if(source == target) return true;
-	for(int i = 0; i < source.adjacent.size(); i++) {
-		GraphNode a = source.adjacent[i];
-		if(!a.visited) {
-			return findViaDFS(a,target);
+	for(int i = 0; i < graph[source].size(); i++) {
+		pair<int,int> node = graph[source][i];
+		if(!visitedNodes[node.first]) {
+			return findViaDFS(node.first,target,graph,visitedNodes);
 		}
 	}
 	return false;
 }
 
-bool pathExists(GraphNode n1,GraphNode n2) {
-	return findViaDFS(n1,n2) || findViaDFS(n2,n1);
-}*/
+/* function: pathExists
+ * -------------------------------------------------
+ * This self explanatory function is a wrapper over
+ * the findViaDFS function. Since the graph is 
+ * directed it may be the case that a path from 
+ * n1 to n2 may exist but not otherwise, or vice 
+ * versa. Thus both checks are necessary.
+ */
+bool pathExists(int n1, int n2, AdjList &graph) {
+	vector<bool> nodes1(graph.size());; // 1 if visited, 0 otherwise
+	vector<bool> nodes2(graph.size());; // 1 if visited, 0 otherwise
+	return findViaDFS(n1,n2,graph,nodes1) || findViaDFS(n2,n1,graph,nodes2);
+}
 
-int main() {
-	freopen("path.txt","r",stdin);
-	int V;
+/* 
+ * function: populateGraph
+ * ---------------------------------------------
+ * Simple utitlity function that populates a graph
+ * given a properly formatted input text
+ */
+void populateGraph(AdjList &graph,char *file) {
+	freopen(argv[1],"r",stdin);
+	int V, total_neighbors, index, weight;
 	scanf("%d",&V);
 	for(int i = 0; i < V; i++) {
-		
+		vector<pair<int,int>> neighbors;
+		scanf("%d ",&total_neighbors);
+		for(int j = 0; j < total_neighbors; j++) {
+			scanf("%d %d",&index,&weight);
+			neighbors[j] = makepair(index,weight);
+		}
+		graph[i] = neighbors;
 	}
+}
+
+int main(int argc, char *argv[]) {
+	if(argc <= 1) {
+		printf("please pass a graph input file");
+		exit(0);
+	}
+	AdjList &graph;
+	populateGraph(graph,argv[1]);
 	return 0;
 }
